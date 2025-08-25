a4bb1e8c826800b772284302c30fbf4b6c966c0b
levine23 <gwangy682@gmail.com>
2025-08-26 01:21:43 +0900

    Update font.py

--- font.py
+++ font.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from YukkiMusic import app
+from damMusic import app
 
 
 class Fonts:
@@ -2550,7 +2550,7 @@ async def style(c, m):
 
 
 __HELP__ = """
-• /font [text] - ᴄᴏɴᴠᴇʀᴛs sɪᴍᴩʟᴇ ᴛᴇxᴛ ᴛᴏ ʙᴇᴀᴜᴛɪғᴜʟ ᴛᴇxᴛ ʙʏ ᴄʜᴀɴɢɪɴɢ ɪᴛ's ғᴏɴᴛ.
+<blockquote>• /font [text] - ᴄᴏɴᴠᴇʀᴛs sɪᴍᴩʟᴇ ᴛᴇxᴛ ᴛᴏ ʙᴇᴀᴜᴛɪғᴜʟ ᴛᴇxᴛ ʙʏ ᴄʜᴀɴɢɪɴɢ ɪᴛ's ғᴏɴᴛ.</blockquote>
  """
 
 __MODULE__ = "Fᴏɴᴛ"
